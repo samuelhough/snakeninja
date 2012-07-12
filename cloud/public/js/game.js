b4c8403650740e4d594b952ed3e309b762e42d58
Richard Harrington <harrington.richard@gmail.com>
2012-07-11 22:51:04 -0400

    both models and views almost done, css remains.

--- cloud/public/js/game.js
+++ cloud/public/js/game.js
@@ -52,10 +52,25 @@ var DIRECTION_TRANSFORM_MAP = {
 	}
 };
 
+var helpers = {
+	oppositeDirection: function(dir) {
+		if (dir === 'north') return 'south';
+		if (dir === 'south') return 'north';
+		if (dir === 'east') return 'west';
+		if (dir === 'west') return 'east';
+	}
+}
+
 
 var CellModel = function(faceIdx, x, y) { 
 	
-	this.state = null;
+	// TODO: Make getter and setter for color.
+	// Also, instead of _state being a string,
+	// it should be a reference to a snake object,
+	// which would then contain a color (or it should be null).
+	
+	this._state = null;
+	this.color = color;
 	
 	// READ-ONLY location object
 	this._location = {
@@ -68,48 +83,72 @@ var CellModel = function(faceIdx, x, y) {
 CellModel.prototype = {
 	getLocation: function() {
 		return this._location;
+	},
+	getState: function() {
+		return this._state;
+	},
+	setState: function(state) {
+		if (state !== this._state) {
+			this._state = state;
+			this.trigger('change')
+		}
+	},
+	isEmpty: function() {
+		return this._state === null;
 	}
 }
 
 asEvented.call(CellModel.prototype);
 
 
-var SnakeModel = function(color, startingCell, direction, cube, otherPlayer) {
+var SnakeModel = function(color, startingCell, direction, cube, isOtherPlayer) {
+	
+	// TODO: FIND SOME WAY NOT TO HAVE TO PASS THE CUBE OBJECT TO THIS 
+	// CONSTRUCTOR. RIGHT NOW WE CAN'T THINK OF ANY OTHER WAY TO 
+	// GET ACCESS TO THE ADJACENTCELL METHOD, WHICH REALLY NEEDS TO BE
+	// A METHOD OF THE CUBE, NOT THE CELL. OTHERWISE HOW COULD IT GET REFERENCES
+	// TO THE OTHER FACES?
 	
 	this.color = color;
 	this.cube = cube;
 	this.direction = direction;
-	this.otherPlayer = otherPlayer;
+	this.isOtherPlayer = isOtherPlayer;
 	
 	// an array of cells.
 	this.body = [];
 	
 	this.body[0] = startingCell;
+	startingCell.setState('snake');
+	startingCell.color = color;
 	console.log(this.body);
+	
+	// TODO: Need an event emitter in here for when the snake crosses over into another face.
 
 }
 
 SnakeModel.prototype = {
 	
 	var self = this,
-		_nextHeadCell;
+		_nextHead;
 	
-	addNewHead: function(cell) {
-		this.body.unshift(cell);
-		cell.state = 'snake';
+	addNewHead: function(newHead) {
+		this.body.unshift(newHead);
+		newHead.setState('snake');
+		newHead.color = this.color;
 	},
 	
 	removeOldTail: function() {
 		var oldTail = this.body.pop();
-		oldTail = null;
+		oldTail.setState(null);
+		oldTail.color = null;
 	},
 	
 	evaluateMove: function() {
 		
 		var newHead = this.cube.adjacentCell(this.body[0], this.direction);
 			
-		if (cube.isEmpty(newHead)) {
-			_nextHeadCell = newHead;
+		if (newHead.isEmpty()) {
+			_nextHead = newHead;
 		} 
 		else {
 			this.trigger('hasHitObstacle');
@@ -117,7 +156,17 @@ SnakeModel.prototype = {
 	}
 
 	move: function() {
-		this.addBodySegment(_nextHeadCell);
+		var currentHeadFaceIdx = this.body[0].location.faceIdx,
+			nextHeadFaceIdx = _nextHead.location.faceIdx;
+			
+		if (currentHeadFaceIdx !== nextHeadFaceIdx) {
+			this.trigger('leavingFace', {
+				from: currentHeadFaceIdx,
+				to: nextHeadFaceIdx
+			});
+		}
+		
+		this.addBodySegment(_nextHead);
 		
 		if (!this.hasJustEaten()) {
 			this.removeOldTail();
@@ -125,9 +174,11 @@ SnakeModel.prototype = {
 	},
 	
 	hasJustEaten: function() {
-		// TODO: put in code for eating food here. (Requires continual 
-		// food generation from the server.) Right now, just make sure 
-		// the snake doesn't get too long.
+		
+		// TODO: put in code for eating food here. (Not urgent.) Requires 
+		// action from the server to make sure that there is always one
+		// bit of food on every face, visible to both players.
+		// Right now, just make sure the snake doesn't get too long.
 		
 		return this.body.length < 100;
 	}
@@ -144,33 +195,47 @@ var SnakeView = function(snake) {
 		socket.emit("PlayerDeath");
 	});
 	
-	if (!snake.otherPlayer) {
-		// Event handlers:
-		$('body').on('keydown', $.proxy(function(e) {
+	socket.on('receivePlayerDirection', function(data) {
+		snake.direction = data.direction;
+	});
+	
+	if (!snake.isOtherPlayer) {
+		
+		// Event handlers for this player:
+		$('body').on('keydown', function(e) {
+			
 			console.log(e.which);
 			if (!GAME_ON) return;
-			if (e.which === 87 && mySnake.direction !== "south"){
-				snake.direction =	 "north";
+			var newDirection;
+			
+			if (e.which === 87) {
+				newDirection = "north";
 			}
-			else if (e.which == 68 && mySnake.direction !==  "west"){
-				snake.direction =	 "east";
+			else if (e.which == 68) {
+				newDirection = "east";
 			}
-			else if (e.which == 83 && mySnake.direction !==  "north"){
-				snake.direction =	 "south";
+			else if (e.which == 83) {
+				newDirection = "south";
 			}
-			else if (e.which == 65 && mySnake.direction !==  "east"){
-				snake.direction =	 "west";
+			else if (e.which == 65) {
+				newDirection = "west";
 			}
 			
-			socket.emit('movePlayer', {
-				direction: snake.direction
-			});
+			if (newDirection && 
+						newDirection !== snake.direction && 
+						newDirection !== helpers.oppositeDirection(snake.direction)) {
+				
+				snake.direction = newDirection;
+				socket.emit('movePlayer', {
+					direction: snake.direction
+				});
+			}
 			
 /*			// quit
 			else if (e.which === 27) {
 				clearInterval(interval);
 			} */
-		}));
+		});
 	}
 }
 
@@ -185,20 +250,92 @@ var FaceModel = function(size, faceIdx) {
 	var fillerArray = _.range(0, size);
 	this.grid = _.map(fillerArray, function (x) {
 		return _.map(fillerArray, function (y) {
-			return new CellModel(faceIdx, x, y);
+			var newCell = new CellModel(faceIdx, x, y);
+			newCell.on('change', $.proxy(this.changeHandler, this));
+			return newCell;
 		});
 	}); 
 }
 
 FaceModel.prototype = {
+	changeHandler: $.proxy(function(cell) {
+		this.trigger('cellChange', cell);
+	}, this),
 	
 }
 
 asEvented.call(FaceModel.prototype);
 
 
-// TODO: add FaceView, which will be a wrapper and event handler and emitter for
-// a single canvas object.
+var FaceView = function(face, el, position) {
+	var size = face.size;
+	
+	// Position is a number from 0 to 5. It starts out corresponding
+	// exactly to the faceView's model's index in the faces array of the CubeModel, 
+	// but instead of being a permanent
+	// ID for the face (which the faces indices are, in the CubeModel),
+	// position is just where the face happens to be located at the moment,
+	// in the UI. (The starting arrangement will be specified in the CSS stylesheet.)
+	
+	this.position = position;
+	
+	this.el = document.getElementById(el);
+	this.el.width = size * BLOCK_HEIGHT;
+	this.el.height = size * BLOCK_HEIGHT;
+	
+	var c = this.ctx = this.el.getContext('2d');
+	
+	// Styling
+	c.lineCap = LINE_CAP;
+	c.lineJoin = LINE_JOIN;
+	c.lineWidth = 1;
+	c.strokeStyle = c.fillStyle = BACKGROUND_COLOR;
+	
+	// Clear canvas
+	c.fillRect( 0, 0, size * BLOCK_HEIGHT, size * BLOCK_HEIGHT);
+	
+	// Set event handler.
+	this.on('cellChange', $.proxy(function(cell) {
+		var location = cell.getLocation();
+		var color = cell.color;
+		if (color) {
+			this.drawCell(location.x, location.y, color);
+		} else {
+			this.eraseCell(location.x, location.y);
+		}
+	}, this));
+	
+}
+
+var FaceView.prototype = {
+	drawCell: function(x, y, color) {
+		var c = this.ctx;
+
+		c.fillStyle = color;
+		var left = x * BLOCK_HEIGHT + 1;
+		var top = y * BLOCK_HEIGHT + 1;
+		c.fillRect( left, top, BLOCK_HEIGHT, BLOCK_HEIGHT );
+		c.strokeRect( left, top, BLOCK_HEIGHT, BLOCK_HEIGHT );			
+	},
+	
+	eraseCell: function(x, y) {
+		this.renderCell(x, y, BACKGROUND_COLOR);
+	},
+	
+	rotate: function(axis) {
+		
+		// The CANVAS_ROTATION_MAP is not made yet. It will consist
+		// of six different rotations, with property names 
+		// 'x-left', 'x-right', 'y-left', 'y-right', 'z-left', and 'z-right',
+		// for left-handed and right-handed spin. Each of these properties
+		// will be containing both the number of the new position for the 
+		// canvas, and the css transforms necessary to get it there.
+		
+		var rotationObj = CANVAS_ROTATION_MAP[axis];
+		this.position = rotationObj.newPosition;
+		$(this.el).css(rotationObj.cssTransforms);
+	}
+}
 
 	
 var CubeModel = function(size){
@@ -216,19 +353,22 @@ CubeModel.prototype = {
 	
 	addSnake: function(snake) {
 		this.snakes.push(snake);
-	},
-	
-	isEmpty: function(cell) {
-		
-		// TODO (not urgent): put in support for not just empty cells and snake cells, but cells with food.
-		
-		var location = cell.getLocation(),
-			faceIdx = location.faceIdx,
-			x = location.x,
-			y = location.y;
+		snake.on('leavingFace', function(transitObj.) {
+			var from = transitObj.from;
+			var to = transitObj.to;
 			
-		return this.faces[faceIdx].grid[x][y].state !== 'snake';
-	}
+			// TODO: Flesh this out. This needs to use the 'from' and 'to' face indices
+			// to determine the axis of rotation (using yet another arbitrary map, probably)
+			// and then it needs to call the 'rotate' method on all six of the faces, using
+			// the axis identifier.
+			
+			// OH NO WAIT! THIS IS ACTUALLY A BIT TRICKIER THAN THAT. THE 'FROM' AND 'TO' INDICES,
+			// AS CURRENTLY WRITTEN, ONLY REFER TO THE MODEL RELATIONSHIPS, NOT TO THE ACTUAL CURRENT
+			// POSITION OF THE FACES AS SEEN BY THE USER. MUST DO SOMETHING ABOUT THAT. PERHAPS STORE DYNAMIC TRANSFORM 
+			// MAPS SO THAT MODEL INDICES CAN ALWAYS BE USED TO FIND CURRENT UI POSITION.
+
+		})
+	},
 	
 	adjacentCell: function(cell, direction) {
 		
@@ -242,6 +382,9 @@ CubeModel.prototype = {
 			newFace = face,
 			new_x = x,
 			new_y = y;
+			
+		// First do the simple calculation, which will be all that is
+		// needed if the snake remains within the face.
 			
 		if (direction === "east") {
 			new_x += 1;
@@ -303,7 +446,7 @@ CubeModel.prototype = {
 			}
 			
 			
-			// TODO: the eight special cases of when it's tilted 90 degrees in either direction.
+			// The eight special cases of when it's tilted 90 degrees in either direction:
 			
 			if (edge === "east" && newEdge === "north") {
 				new_x = size - y - 1;
@@ -333,11 +476,6 @@ CubeModel.prototype = {
 var CubeView = function(cube) {
 	this.cube = cube;
 	
-	// TODO: Flesh this out. This will be the thing that renders all the canvas
-	// objects, and keeps them in proper orientation to each other, and to the viewer..
-
-
-	
 }
 
 CubeView.prototype = {
@@ -350,71 +488,32 @@ CubeView.prototype = {
 				snake.move();
 			});
 		}
-	}
-}
-
-// TODO: cannibalize all functionality from the rest of this document
-// (or at lest from the canvas constructor) and put it into various views above.
-
-var Canvas = function(elementId, field, size) {		
-	var self = this;
-	
-	this.size = size;
-	
-	this.el = document.getElementById(elementId);
-	this.el.width = size * BLOCK_HEIGHT;
-	this.el.height = size * BLOCK_HEIGHT;
-	
-	var c = this.ctx = this.el.getContext("2d");
-	
-	// Styling
-	c.lineCap = LINE_CAP;
-	c.lineJoin = LINE_JOIN;
-	c.lineWidth = 1;		
-	c.strokeStyle = c.fillStyle = BACKGROUND_COLOR;
-
-	
-	// Clear screen
-	c.fillRect( 0, 0, size * BLOCK_HEIGHT, size * BLOCK_HEIGHT);
-	
-	var drawCell = function(location, color) {
-		
-		c.fillStyle = color;
-		var left = location.x * BLOCK_HEIGHT + 1;
-		var top = location.y * BLOCK_HEIGHT + 1;
-		c.fillRect( left, top, BLOCK_HEIGHT, BLOCK_HEIGHT );
-		c.strokeRect( left, top, BLOCK_HEIGHT, BLOCK_HEIGHT );			
-	}
-	this.drawCell = drawCell;
-	
-	eraseCell = function(location) {
-		c.fillStyle = BACKGROUND_COLOR;
-		var left = location.x * BLOCK_HEIGHT + 1;
-		var top = location.y * BLOCK_HEIGHT + 1;
-		c.fillRect( left, top, BLOCK_HEIGHT, BLOCK_HEIGHT );
-	}
-	
-	var render = function() {
-		_.each(field.snakes, function(snake) {
-			var c = self.ctx;	
-
-			drawCell(snake.body[0], snake.color);	
-			
-		});
-	};
-	
-	var step = function() {
-		// Renders first, so we don't even need to have a startSnake method.
-		render();
-		field.step();
-	}
-	
-	this.init = function(){
-		setInterval(function(){
-			step();
+	},
+	init: function() {
+		setInterval(function() {
+			this.step();
 		}, INTERVAL_TIME);
 	}
 }
+
+
+
+
+
+
+// THE REST OF THIS FROM HERE TO THE END IS PARTIALLY REWRITTEN TO TAKE ACCOUNT OF THE 
+// NEW CONSTRUCTORS ABOVE. THAT JUST NEEDS TO BE FINISHED.
+
+// THEN THE ROTATION MAP NEEDS TO BE WRITTEN, AND THE INITIAL CSS TRANSFORM AND TRANSITION ATTRIBUTES, AND THE
+// SNAKE EVENT THAT CAUSES THE CUBE TO ROTATE. ALSO THE LISTENER FOR THAT EVENT, IN THE CUBE.
+
+// THEN I THINK WE'RE DONE!
+
+
+
+
+
+
 
 // Once we receive the player colors, everything else can happen.
 
@@ -424,24 +523,11 @@ var mySnake,
 	cube = new CubeModel(GRID_SIZE, FIELDNUM),
 	canvas = new Canvas('mainCanvas', field, GRID_SIZE);
 	
-<<<<<<< HEAD
 socket.on('thisPlayerData', function(data){
 	var color = data.color,
 		location = data.location,
 		cell = cube.faces[location.faceIdx].grid[location.x][location.y],
 		direction = (location.y < 20) ? "south" : "north";
-=======
-	// Event handlers:
-	// (THIS FIRST ONE IS TEST ONLY):	
-	$('body').on('keydown', function(e) {
-		cookie.reset();
-		console.log(e.which);
-		if(!GAME_ON){ return; };
-		if(e.which === 87 && mySnake.direction !== DIRECTIONS["south"]){
-			mySnake.direction =  DIRECTIONS["north"];
-	//		socket.emit('movePlayer', {dir: 'north'});
-		}
->>>>>>> ad7265dfe6de01dbd92f44789eed5240d070df44
 		
 		console.log(location);
 		
